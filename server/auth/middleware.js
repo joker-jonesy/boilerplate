c9fe2c5defe6544bb6bebcc13a66dc686aabfe36
joker-jonesy <spashmcgrath94@yahoo.com>
2023-10-03 16:29:25 -0400

    modified redux/middleware

--- server/auth/middleware.js
+++ server/auth/middleware.js
@@ -1,20 +1,16 @@
 const jwt = require("jsonwebtoken");
 const process = require("process");
 const protection = async (req, res, next) => {
-    console.log("auth middleware invoked")
     const token = req.headers.authorization
 
-    console.log("extracted token:", token)
     if (!token) {
         return res.status(401).send("No token provided.");
     }
 
     try {
         req.user = jwt.verify(token, process.env.JWT);
-        console.log("middleware req.user", req.user)
         next();
     } catch (error) {
-        console.log("Token verification error:", error.message);
         return res.status(403).send("Failed to authenticate token.")
     }
 };
--- src/reducers/api.js
+++ src/reducers/api.js
@@ -1,10 +1,27 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
+const CREDENTIALS = "credentials";
+
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
     tagTypes:['tag'],
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({baseUrl: "localhost:3000"}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: "localhost:3000",
+        prepareHeaders: (headers, { getState }) => {
+            console.log("prepareHeaders is running");
+
+            const credentials = window.sessionStorage.getItem(CREDENTIALS);
+            const parsedCredentials = JSON.parse(credentials || "{}");
+            const token = parsedCredentials.token;
+            console.log("token from reducer", token);
+            if (token) {
+                headers.set("Authorization", token);
+            }
+            console.log("token from session storage:", token);
+            return headers;
+        },
+    }),
     endpoints: (builder) => ({
 
 
